dd51da38671c92e55539c7227a7cfbc60623b1d6
Isik <isik@Isiks-iMac-2.local>
2026-01-01 12:53:42 -0500

    Force icon refresh

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export const metadata: Metadata = {
 
     icons: {
       icon: [
-        { url: "/favicon.ico?v=3" }, // important
         { url: "/favicon-32x32-v2.png", sizes: "32x32", type: "image/png" },
         { url: "/favicon-16x16-v2.png", sizes: "16x16", type: "image/png" },
       ],
